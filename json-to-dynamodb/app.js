41075318eed7b97b51c962a438284f3bb4716bae
John Peebles <johnpeeb@amazon.com>
2021-03-24 12:18:53 -0700

    Add dynamic search fields and primary key, assuming primary key will be "id"

--- json-to-dynamodb/app.js
+++ json-to-dynamodb/app.js
@@ -1,8 +1,10 @@
 let response;
 const fileToImport = require("./fileToImport.json");
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = process.env.TABLE_NAME;
+const PRIMARY_KEY = process.env.PRIMARY_KEY;
+const SEARCHABLE_FIELDS = process.env.SEARCHABLE_FIELDS;
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -16,26 +18,60 @@ const TABLE_NAME = process.env.TABLE_NAME;
  *
  */
 exports.lambdaHandler = async (event, context) => {
-    let promises = [];
-    try {
-        fileToImport.forEach(item => {
-            const params = {
-                TableName: TABLE_NAME,
-                Item: item,
-            };
-            promises.push(dynamoDb.put(params).promise());
-        });
-        await Promise.all(promises);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'File Imported',
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
+  const chunk = (array, size) =>
+    array.reduce((acc, _, i) => {
+      if (i % size === 0) {
+        acc.push(array.slice(i, i + size));
+      }
+      return acc;
+    }, []);
 
-    return response
+  const schema = {
+    searchableFields: SEARCHABLE_FIELDS,
+    primaryKey: PRIMARY_KEY,
+    tableName: TABLE_NAME,
+  };
+
+  const searchableItem = (item, primaryKey, searchableFields) => {
+    searchableFields.map((field) => {
+      item[`${field}Search`] = item[field].toLowerCase();
+    });
+    item["id"] = item[primaryKey];
+    return item;
+  };
+
+  let promises = [];
+  try {
+    const chunked = chunk(fileToImport, 25);
+    for (const chunk of chunked) {
+      const chunkedItems = [];
+      for (const chunkedItem of chunk) {
+        const item = {
+          PutRequest: {
+            Item: searchableItem(
+              chunkedItem,
+              schema.primaryKey,
+              schema.searchableFields
+            ),
+          },
+        };
+        chunkedItems.push(item);
+      }
+      const params = { RequestItems: {} };
+      params["RequestItems"][TABLE_NAME] = chunkedItems;
+      promises.push(dynamoDb.batchWrite(params).promise());
+    }
+    await Promise.all(promises);
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "File Imported",
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+  return response;
 };
